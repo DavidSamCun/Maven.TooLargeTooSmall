e99ed74379c5682eb2d21b2f89783d4fd80ff05f
DavidCun <99755247+DavidSamCun@users.noreply.github.com>
2022-02-25 08:06:41 -0500

    need to method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,15 +9,26 @@ public class Main {
     public static void main(String[] args){
 
         Random rnd = new Random(); //Declare random
-        int mystery = rnd.nextInt(1,10); //Random number between 1 and 100
+        int mystery;
         int guess;
         int tries = 1;
+        int min;
+        int max;
         //System.out.println("Random number is " + mystery); //Check if random is working
 
-        System.out.println("Take a guess...");
-        Scanner num = new Scanner(System.in);
+        System.out.println("Enter the lowest number...");
+        Scanner num1 = new Scanner(System.in);
+        min = num1.nextInt();
 
-        guess = num.nextInt();
+        System.out.println("Enter the highest number...");
+        Scanner num2 = new Scanner(System.in);
+        max = num2.nextInt();
+        mystery = rnd.nextInt(min,max); //Random number between 1 and 100
+
+        System.out.println("Take a guess...");
+        Scanner num3 = new Scanner(System.in);
+
+        guess = num3.nextInt();
 
         if(guess == mystery){
             System.out.println("Congrats, you got it on the first try! It's " + mystery);
@@ -25,13 +36,13 @@ public class Main {
             for (int i = 1; guess != mystery; i++) {     //will keep running until it's true
                 if (guess < mystery) {
                     System.out.println("Too low, try again...");
-                    Scanner num1 = new Scanner(System.in);
-                    guess = num1.nextInt();
+                    Scanner num4 = new Scanner(System.in);
+                    guess = num4.nextInt();
                     tries += 1;
                 } else if (guess > mystery) {
                     System.out.println("Too high, try again...");
-                    Scanner num2 = new Scanner(System.in);
-                    guess = num2.nextInt();
+                    Scanner num5 = new Scanner(System.in);
+                    guess = num5.nextInt();
                     tries += 1;
                     //} else if (guess == mystery){
                     //    System.out.println("You got it! The number was " + mystery + "You got it in " + i + " tries.");
